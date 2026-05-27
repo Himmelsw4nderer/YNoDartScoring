610a7addf95b0bff598e78339711fc0e4e3ae7d4
nbax <Nils>
2026-05-27 21:35:34 +0200

    Update score_board.rs

--- frontend/src/components/game/score_board.rs
+++ frontend/src/components/game/score_board.rs
@@ -1,5 +1,6 @@
 use yew::prelude::*;
 use crate::components::game::score_card::ScoreCard;
+use crate::components::game::collapsed_score_card::CollapsedScoreCard;
 use crate::models::GameState;
 use crate::log_error;
 
@@ -33,10 +34,13 @@ pub fn score_board() -> Html {
         }
     } else {
         html! {
-            <div class="score-board grid gap-4 grid-cols-3">
-                { for (0..player_count).map(|i| html! {
-                    <div class="w-full"><ScoreCard player_index={i} /></div>
-                }) }
+            <div class="score-board flex gap-4">
+                <div class="w-1/2"><ScoreCard player_index={game_state.current_player} /></div>
+                <div class="w-full flex flex-wrap gap-2">
+                    { for game_state.players.iter().enumerate().filter(|(i, _)| *i != game_state.current_player).map(|(i, _)| html! {
+                        <CollapsedScoreCard player_index={i} />
+                    }) }
+                </div>
             </div>
         }
     }
